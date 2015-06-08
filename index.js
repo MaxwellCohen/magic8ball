d59d8d92b57e6579a62df33189ee993ce2cc00f2
Maxwell Cohen <to.email.max@gmail.com>
2015-06-08 18:13:08 -0500

    fixed issue with jquery access over http

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use('/jquery',express.static(__dirname + '/jquery/dist'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World this is max!');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
